6d976c97223f8b99e0075dba20b4e2d1f06ef98e
Oluwamayowa George <themayowageorge@gmail.com>
2021-04-22 17:32:02 +0100

    Added React Router and Logo

--- personalpage/src/App.js
+++ personalpage/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import IntroPage from './components/IntroPage/IntroPage';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import "./App.css";
+import IntroPage from "./components/IntroPage/IntroPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Marketing from "./components/marketing/Marketing";
 
 function App() {
   return (
     <>
       <Router>
-        <Route path='/'> <IntroPage /> </Route>
+        <Route exact path="/">
+          {" "}
+          <IntroPage />{" "}
+        </Route>
+        <Route exact path="/marketing">
+          {" "}
+          <Marketing />{" "}
+        </Route>
       </Router>
     </>
   );
---
+++ personalpage/src/components/marketing/Marketing.js
@@ -0,0 +1,20 @@
+import styles from './Marketing.module.scss'
+import logo from '../media/logo.png';
+
+const Marketing = () => {
+
+    return (
+        <>
+            <div>
+                <nav>
+                    <div><img src={logo} alt="logo"></img></div>
+                    <div>
+
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default Marketing;
