3ebeeb5210dc52ad9d196a4191747ecb3c9c11c4
Oluwamayowa George <themayowageorge@gmail.com>
2021-04-25 04:20:30 +0100

    Updated background image

--- personalpage/src/components/marketing/Marketing.js
+++ personalpage/src/components/marketing/Marketing.js
@@ -1,30 +1,41 @@
-import styles from './Marketing.module.scss'
-import logo from '../media/logo.png';
+import styles from "./Marketing.module.scss";
+import logo from "../media/pagelogo.png";
 
 const Marketing = () => {
-
-    return (
-        <>
-            <div>
-                <nav>
-                    <div className={styles.logobox}><img src={logo} alt="logo"></img></div>
-                    <div>
-                        <ul className={styles.navlist}>
-                            <li>ABOUT</li>
-                            <li>SERVICES</li>
-                            <li>WORK</li>
-                        </ul>
-                    </div>
-                </nav>
-                <div>
-                    <div>
-                        <h2>Marketing that works, from Strategy to Execution</h2>
-                        <p>Growing and scaling your business isn't a science. It's an art. And I'm Pablo fucking Picasso.</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+      <header className={styles.header}>
+        <nav>
+          <div className={styles.logobox}>
+            <img src={logo} alt="logo"></img>
+          </div>
+          <div>
+            <ul className={styles.navlist}>
+              <li>ABOUT</li>
+              <li>SERVICES</li>
+              <li>WORK</li>
+            </ul>
+          </div>
+        </nav>
+        <div className={styles.hero}>
+          <div className={styles.herotext}>
+            <h2>Marketing that works,<br></br> from Strategy to Execution</h2>
+            <p>
+              I'm a full-stack marketer that understands Product, Digital,
+              Content and Growth marketing, and how to make them work for your business.
+            </p>
+          </div>
+          <div className={styles.buffer}></div>
+          </div>
+        </header>
+        <section>
+            <div></div>
+            <div></div>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default Marketing;
